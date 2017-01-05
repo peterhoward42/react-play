42d3e4e2e21e8a3eba40c8aade7c6d3752ae7a2a
peterhoward42 <peterhoward42@gmail.com>
2017-01-05 18:15:17 +0000

    started playing with jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App, MainPage } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+
+describe('Testing plain js', () => {
+  it('can divide', function () {
+    expect(12 / 4).toEqual(3)
+  })
+  it('can square', function () {
+    expect(8 * 8).toEqual(64)
+  })
+})
+
+
+describe('Exercise a class method', () => {
+  it('filters the words correctly', function () {
+    const mainPage = new MainPage()
+    const words = mainPage.filteredWords()
+    const nWords = words.length
+  })
+})
+
+describe('Exercising the rendering of a React element', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  })
+})
+

4c32b681940c51c963d23319d99604f71a30cdb0
peterhoward42 <peterhoward42@gmail.com>
2017-01-12 21:18:36 +0000

    wip

--- src/redux_conceptual.js
+++ src/redux_conceptual.js
@@ -17,16 +17,21 @@ let kitchen = {
     occupiedBy: ['peter', 'ali'],
 }
 
-// We define some actions; things you can do to the kitchen
+// We define some action verbs; things you can do to the kitchen
+// Custom is to describe them in past tense I believe.
 
-const TURN_KETTLE_OFF = 'turn kettle off'
-const TURN_KETTLE_ON = 'turn kettle on'
-const TURN_ALL_ELECTRICAL_OFF = 'turn all electrical off'
+const TURNED_KETTLE_OFF = 'turn kettle off'
+const TURNED_KETTLE_ON = 'turn kettle on'
+const TURNED_ALL_ELECTRICAL_OFF = 'turn all electrical off'
 const SOMEONE_LEFT_THE_KITCHEN = 'someone left the kitchen'
 
-const action_kettle_off = { type: TURN_KETTLE_OFF }
-const action_turn_all_electrical_off = { type: TURN_ALL_ELECTRICAL_OFF }
-const action_someone_left_the_kitchen = { type: SOMEONE_LEFT_THE_KITCHEN, who: 'peter' }
+// Now we define some formal ACTIONS
+
+// Note the type field is mandatory if you want to use the real Redux.
+
+const action_turned_kettle_off = { type: TURNED_KETTLE_OFF }
+const action_turned_all_electrical_off = { type: TURNED_ALL_ELECTRICAL_OFF }
+const action_someone_left_the_kitchen = { type: SOMEONE_LEFT_THE_KITCHEN, who: 'peter' } // Carries payload nouns
 
 // And we write reducers - pure functions that interpret actions for a chunk of the state.
 // By definition these deal with the value for one of the state's keys.
@@ -39,15 +44,15 @@ const action_someone_left_the_kitchen = { type: SOMEONE_LEFT_THE_KITCHEN, who: '
 // a reference to the existing, unchanged state, or a completely new state
 // object. This one refers to the value for the electrical.kettleOn state.
 
-function turnKettleOnOrOff(state = false, action) {
+function turnedKettleOnOrOff(state = false, action) {
     switch (action.type) {
-        case TURN_KETTLE_OFF:
+        case TURNED_KETTLE_OFF:
             return false
             break
-        case TURN_KETTLE_ON:
+        case TURNED_KETTLE_ON:
             return true
             break
-        case TURN_ALL_ELECTRICAL_OFF:
+        case TURNED_ALL_ELECTRICAL_OFF:
             return true
             break
         default:
@@ -60,8 +65,8 @@ function turnKettleOnOrOff(state = false, action) {
 // Note this one provides an empty default value, not being a leaf node.
 function turnElectricalsOnOrOff(state = {}, action) {
     return {
-        kettleOn: turnKettleOnOrOff(state.kettleOn),
-        toasterOn: turnToasterOnOrOff(state.toasterOn), 
+        kettleOn: turnKettleOnOrOff(state.kettleOn, action),
+        toasterOn: turnToasterOnOrOff(state.toasterOn, action),
     }
 }
 
@@ -73,5 +78,79 @@ function updateKitchen(state = {}, action) {
         occupiedBy: updateOccupiedBy(state.occupiedBy, action),
     }
 }
+
+// Redux has a helper function to do this for you...
+// But I think it requires that the reducers for each key have the same name as the key...
+// It's wonderfull magic that requires only that you provide  the names of the keys.
+
+const updateKitchen = combineReducers({
+    electrical,
+    dishes,
+    occupiedBy
+})
+
+// Consider actions
+
+// Actions are immutable, so we normally use action creator functions to provide parameterisation
+
+function someoneLeavesTheKitchen(whoLeft) {
+    // The action we create is an immutable thing, constructed with the parameter.
+    return {
+        type: SOMEONE_LEFT_THE_KITCHEN,
+        who: whoLeft,
+    }
+}
+
+// Now we start using the real redux.
+
+// Our states (as they move through time) live in the redux store,
+// and to stimualte state transitions, we 'dispatch' actions to the
+// store using redux global functions.
+// We skipped creating the store in this example.
+
+dispatch(someoneLeavesTheKitchen('fred'))
+
+// Or create a 'bound' version - thus. A bound version is a version that does the dispatch for you.
+const boundSomeoneLeavesTheKitchen = (whoLeft) => dispatch(someoneLeavesTheKitchen('fred'))
+
+// Ways redux makes things simpler
+
+// First of all, use an immutability helper library such as https://github.com/kolodny/immutability-helper
+
+// Now thinking about the store.
+
+// To recap the store is the single owner of the complete state tree, including
+// each new version of it.
+
+// You initialise it with your top level reducer - which is sufficient to initialise the
+// data within because the reducers are able to create default values when none is defined.
+
+let store = createStore(topLevelReducer) // the docs name the top level reducer as the theFooApp
+
+// Or you can re-hydrate the initial state from something you serialized
+let store = createStore(topLevelReducer, initialState)
+
+// We can now send it update mandates by dispatching actions to it.
+// Or ask about the current state using getState()
+// Next we'll look at how you can registers observers to state changes using the subscribe call.
+
+// Register a listener to any state changes
+let unsubscribeFn = store.subscribe(() => {
+    store.getState() // do something with it
+})
+
+// then when done with responding to published events...
+unscribeFn()
+
+// Now consider connecting this lot to a React app
+
+// We (are encouraged) to split our react components into two buckets - containers vs presenational
+// The former are oblivious to React, should have not state, read only from props, write only via injected callbacks.
+// The latter are coupled to react. They subscribe to events and dispatch actions to the store.
+// The latter are usuall auto generated by connect(). Which (presumably) is where the re-rendering gets stimulated?
+
+
+
+
 
 export { kitchen }
